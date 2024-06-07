b8fedee8067cf3927e83f60e906dff7d31b0e471
hong le <vuanhde@yahoo.de>
2024-06-07 10:02:05 +0200

    19.commit am 07.06.24

--- src/test/java/com/hong/demo/MvcJdbcRestApiHApplicationTests.java
+++ src/test/java/com/hong/demo/MvcJdbcRestApiHApplicationTests.java
@@ -1,14 +1,30 @@
 package com.hong.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-// import java.lang.String;
+// import java.util.Date;
+// import java.util.List;
+// import java.util.Collections;
 
-import org.junit.jupiter.api.Test;
 import org.apache.commons.codec.binary.Base64;
+
+// import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+// import org.junit.jupiter.api.AfterAll;
+// import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// https://www.baeldung.com/introduction-to-assertj
+// import static org.assertj.core.api.Assertions.*; 
 import static org.assertj.core.api.Assertions.assertThat;
-// import static org.assertj.core.api.Assertions.*; // https://www.baeldung.com/introduction-to-assertj
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,15 +42,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.HttpStatus;
 
 import com.hong.demo.domain.Book;
-import com.hong.demo.exceptions.ErrorDetails;
+// import com.hong.demo.exceptions.ErrorDetails;
 
 // import org.skyscreamer.jsonassert.JSONAssert;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
+// import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import java.io.IOException;
-import org.json.JSONObject;
-import org.json.JSONException;
+// import java.io.IOException;
+// import org.json.JSONObject;
+// import org.json.JSONException;
+
+// import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 // @SpringBootTest
 // class MvcJdbcRestApiHApplicationTests {
@@ -45,9 +63,11 @@ import org.json.JSONException;
 
 // }
 
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(OrderAnnotation.class)
 class MvcJdbcRestApiHApplicationTests {
+
+	// private final String ROOT_URL = "http://localhost:8000";
 
 	@LocalServerPort
 	private int port;
@@ -58,15 +78,43 @@ class MvcJdbcRestApiHApplicationTests {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	/*
-    * Add HTTP Authorization header, using Basic-Authentication to send user-credentials.
-    */
-	private static HttpHeaders getHeaders2(){   // header for Basic-Authentication
-    	String plainCredentials= "autor:autor"; // name:password
+	// @BeforeEach 
+    // void init() {
+    //     LOG.info("startup");
+    //     list = new ArrayList<>(Arrays.asList("test1", "test2"));
+    // }
+
+    // @AfterEach
+    // void teardown() {
+    //     LOG.info("teardown");
+    //     list.clear();
+    // }
+
+	// @BeforeAll
+    // static void setup() {
+	// 	   LOG.info("startup - creating DB connection");
+	// }
+
+	// @AfterAll
+    // static void tearDown() {
+    //     LOG.info("closing DB connection");
+    // }
+
+	private String getRootUrl(){
+		return "http://localhost:"+port;
+	}
+
+	private String getBase64Credentials(String username, String password){
+		String plainCredentials = username + ":" + password; // "autor:autor"
     	String base64Credentials = new String(Base64.encodeBase64(plainCredentials.getBytes()));
-    	
+		return base64Credentials;
+	}
+
+	// Add HTTP Authorization header, using Basic-Authentication to send user-credentials.
+	private HttpHeaders getHeaders2(){
+    	String base64Credentials = getBase64Credentials("autor", "autor");
+
     	HttpHeaders headers = new HttpHeaders();
-        
     	// headers.add("Authorization", "Basic " + base64Credentials);
 		headers.set("Authorization", "Basic " + base64Credentials);
     	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -76,29 +124,41 @@ class MvcJdbcRestApiHApplicationTests {
 
 	private HttpHeaders getHeaders(){
 		HttpHeaders headers = new HttpHeaders();
-		headers.setBasicAuth("autor", "autor"); // name:password
+		headers.setBasicAuth("autor", "autor"); // username:password
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));		
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		return headers;
 	}
 
-	private String getRootUrl(){
-		return "http://localhost:"+port;
+	@Test
+	@Order(1)
+	public void testAddBook() {
+		Book book = new Book();
+		book.setTitle("test book1");
+		book.setContent("test book1");
+		book.setCreatedOn(null);
+
+		HttpHeaders requestHeaders = getHeaders();  
+
+		HttpEntity<Book> request = new HttpEntity<Book>(book, requestHeaders);
+
+		Book book2 = restTemplate.postForObject(getRootUrl()+"/api/books", request, Book.class);
+		// System.out.println(book2);
+
+		assertThat(book2.getTitle()).isEqualTo("test book1");
 	}
 
 	@Test
-	public void testCreateBook(){
+	@Order(2)
+	public void testAddBook2() {
 
 		Book book = new Book();
-		book.setTitle("Exploring SpringBoot3");
-		book.setContent("SpringBoot 3 is awesome!");
+		book.setTitle("Exploring SpringBoot");
+		book.setContent("SpringBoot was awesome!");
 		book.setCreatedOn(null);
 
-		// HttpHeaders requestHeaders = getHeaders2();
-		// HttpEntity<Object> request = new HttpEntity<Object>(book, requestHeaders);
-
-		HttpHeaders requestHeaders = getHeaders();
-		HttpEntity<Book> request = new HttpEntity<Book>(book, requestHeaders);
+		HttpHeaders requestHeaders = getHeaders2();
+		HttpEntity<Object> request = new HttpEntity<Object>(book, requestHeaders);
 
 		ResponseEntity<String> response = restTemplate.exchange(
 			getRootUrl()+"/api/books", 
@@ -107,38 +167,89 @@ class MvcJdbcRestApiHApplicationTests {
 			String.class
 		);
 
-		// System.out.println(response.getBody());
-		assertThat(response.getStatusCodeValue()).isEqualTo(201);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
 		try {
+			// System.out.println(response.getBody());
 			// JSON to Java mapping code
 			Book book2 = objectMapper.readValue(response.getBody(), Book.class);
-			assertThat(book2.getTitle()).isEqualTo("Exploring SpringBoot3");
-		}  catch (JsonProcessingException e) {
-			System.out.println("Error occurred during JSON processing: " + e.getMessage());
-		}
+			// System.out.println(book2);
 
-		// try {
-		// 	JSONObject jo = new JSONObject(response.getBody());
-		// 	assertThat(jo.getString("title")).isEqualTo("Exploring SpringBoot3");
-		// } catch (JSONException e) {
-		// 	System.out.println("Error occurred during JSON parsing: " + e.getMessage());
-		// } 
+			assertThat(book2.getTitle()).isEqualTo("Exploring SpringBoot");
+			assertEquals("SpringBoot was awesome!", book2.getContent());
+
+		} catch (JsonProcessingException e) {
+			System.out.println("Error occurred during JSON processing: " + e.getMessage());
+		} 
 
 	}
 
 	@Test
+	@Order(3)
+	public void testAddBook3() {
+
+		Book book = new Book();
+		book.setTitle("Exploring SpringBoot3");
+		book.setContent("SpringBoot 3 is awesome!");
+		book.setCreatedOn(null);
+
+		HttpHeaders requestHeaders = getHeaders();
+
+		HttpEntity<Book> request = new HttpEntity<Book>(book, requestHeaders);
+		// HttpEntity<Object> request = new HttpEntity<Object>(book, requestHeaders);
+
+		ResponseEntity<Book> response = restTemplate.exchange(
+			getRootUrl()+"/api/books", 
+			HttpMethod.POST, 
+			request, 
+			Book.class
+		);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		// System.out.println(response.getBody());
+		Book book2 = response.getBody();
+
+		assertThat(book2.getTitle()).isEqualTo("Exploring SpringBoot3");
+		assertEquals("SpringBoot 3 is awesome!", book2.getContent());
+
+	}
+
+	@Test
+	@Order(4)
+	// @AfterAll // method must static
 	public void testGetAllBooks(){	
 		HttpHeaders headers = getHeaders();
-		HttpEntity<String> entity = new HttpEntity<>(null, headers);
-		ResponseEntity<String> response = restTemplate.exchange(
+		HttpEntity<?> entity = new HttpEntity<>(null, headers);
+		// HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+		ResponseEntity<Iterable<Book>> response = restTemplate.exchange(
 			getRootUrl()+"/api/books", 
 			HttpMethod.GET, 
 			entity, 
-			String.class
+			new ParameterizedTypeReference<Iterable<Book>>(){}
 		);
-		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-		assertThat(response.getBody()).hasSizeGreaterThan(0);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		// System.out.println(response.getBody());
+		Iterable<Book> list = response.getBody();
+		assertThat(list).hasSize(3);
+
+		//-------------------
+
+		// ResponseEntity<String> response = restTemplate.exchange(
+		// 	getRootUrl()+"/api/books", 
+		// 	HttpMethod.GET, 
+		// 	entity, 
+		// 	new ParameterizedTypeReference<String>(){}
+		// );
+
+		//-------------------
+		// ResponseEntity<String> response = restTemplate
+		// .withBasicAuth("autor", "autor")
+		// .getForEntity(getRootUrl()+"/api/books", String.class);
+		// System.out.println(response.getBody());
+ 
 	}
 
 	// @Test
@@ -146,7 +257,12 @@ class MvcJdbcRestApiHApplicationTests {
 	// 	String bookId = "5";
 	// 	HttpHeaders requestHeaders = getHeaders();
 	// 	HttpEntity<String> request = new HttpEntity<>(requestHeaders);
-	// 	ResponseEntity<ErrorDetails> responseEntity = restTemplate.exchange(getRootUrl()+"/api/books/"+bookId, HttpMethod.DELETE, request, ErrorDetails.class);
+		// ResponseEntity<ErrorDetails> responseEntity = restTemplate.exchange(
+		// // getRootUrl()+
+		// "/api/books/"+bookId, 
+		// HttpMethod.DELETE, 
+		// request, 
+		// ErrorDetails.class);
 	// 	assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
 	// }
 
